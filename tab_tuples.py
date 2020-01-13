410007b5b1cabfb1f479aff2efb128a61a276738
LeilaYin <you@example.com>
2020-01-13 15:18:56 +0100

    squelette de la classe tab_tuples

---
+++ tab_tuples.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 14:16:24 2020
+
+@author: leila.nedjar
+"""
+
+class tab_tuples :
+    
+    def __init__(self): 
+        pass
+        
+    def __init__(self, tab): 
+        pass
+    
+    def ajoute(self, element): 
+        pass
+    
+    def somme_totale(self): 
+        pass
+    
+    def somme_partielle(self): 
+        pass
+    
+if __name__ == "__main__":
+    pass
