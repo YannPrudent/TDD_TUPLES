9987632861902c08645c37abe59d9e60b269241d
rvende <vende.remi@hotmail.fr>
2020-01-14 14:59:50 +0100

    resolution conflit

--- tab_tuples.py
+++ tab_tuples.py
@@ -6,8 +6,20 @@ Created on Mon Jan 13 14:16:24 2020
 @author: leila.nedjar
 """
 class tab_tuples :
+    """
+        Cette classe permet d'avoir un tableau de tuples 
+        et les fonctions qui lui sont associées pour travailler avec ce tableau
+    """
 
     def __init__(self, tab = None):
+        """
+            Cette fonction est le constructeur,
+            :param tab: Le constructeur peut être appelé sans paramètres, auquel cas on construit un tableau de tuple vide.
+            Si le constructeur est appelé avec un paramètre, qui doit être un tableau de tuple non vide, 
+            dans ce cas on recopie le tableau passé en argument dans le tableau de tuples de notre classe.
+            :type tab: tableau de tuple.
+            
+        """
         if tab is None:
             self.tab = []
         else :
@@ -25,6 +37,15 @@ class tab_tuples :
             self.tab = tab
     
     def ajoute(self, element):
+        """
+            Cette fonction ajoute un élément à notre tableau. 
+            :param element: Elle prend en paramètre un élément de deux entiers positifs.
+            :type element: tuple.
+            Il y a des contraintes à l'ajout d'élément qui sont les suivants : 
+                - Les entiers du tuple à ajouter ne peuvent être negatifs.
+                - La somme des deux entiers du tuple à ajouter ne peut être supérieure à 10
+                - L'élément doit avoir une taille de 2 (tuple de 2 entiers positifs)
+        """
         if len(element) is not 2:
             raise Exception("L'élément que vous essayer d'ajouter est a une taille differente de 2 ")
         if element[0] < 0 or element[1] < 0:
@@ -34,6 +55,12 @@ class tab_tuples :
         self.tab.append(element)
     
     def supprime(self,index):
+        """
+            Cette fonction supprime un élément du tableau.
+            :param index: Elle prend en paramètre un index, correspondant à la case que l'on souhaite supprimer
+            :type index: int
+            On va venir supprimer l'élément à l'index donné en paramètre.
+        """
         if len(self.tab) is 0 :
             raise Exception ("Le tableau est vide")
         if index > len(self.tab)-1 or index < 0:
@@ -43,6 +70,13 @@ class tab_tuples :
         
     
     def getTuple(self, index):
+        """
+            Cette fonction renvoie un tuple.
+            :param index: Elle prend en paramètre un index correspondant à une case du tableau de tuple.
+            :type index: int
+            :returns: La fonction renvoie donc le tuple correspondant à l'index donnée.
+            :rtype: tuple
+        """
         if len(self.tab) is 0 :
             raise Exception ("Le tableau est vide")
         if index > len(self.tab)-1 or index < 0:
@@ -51,6 +85,11 @@ class tab_tuples :
             return self.tab[index]
     
     def somme_totale(self):
+        """
+            Cette fonction effectue la somme totale de chaque éléments de chaque tuple de notre tableau de tuple.
+            :returns: La fonction renvoie la somme totale de chaque éléments de chaque tuple de notre tableau de tuple.
+            :rtype: int.
+        """
         if len(self.tab) is 0 :
             raise Exception("Le tableau de tuples est vide ")
         res = 0
@@ -64,6 +103,12 @@ class tab_tuples :
         return res
     
     def somme_partielle(self):
+        """
+            Cette fonction effectue la somme partielle de notre tableau de tuple.
+            Elle va aditionner les éléments de chaque tuple un à un et renvoyer un tableau contenant la somme de chaque tuple.
+            :returns: tableau contenant la somme de chaque tuple.
+            :rtype: tableau de int
+        """
         resTab = []
         if len(self.tab) is 0 :
             raise Exception("Le tableau de tuples est vide ")
